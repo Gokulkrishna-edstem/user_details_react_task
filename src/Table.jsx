52fb4c704bc40402db4d67137489e946758372f4
Gokulkrishna-edstem <gokulkrishna.ab@edstem.com>
2025-03-13 11:41:47 +0530

    made few changes

--- src/Table.jsx
+++ src/Table.jsx
@@ -71,58 +71,76 @@ const data = {
   ],
 };
 
+function Head() {
+  return (
+    <thead className="row tableHead">
+      <tr>
+        <th align="center" className="items">
+          Id
+        </th>
+        <th align="center" className="items">
+          Name
+        </th>
+        <th align="center" className="items">
+          Email
+        </th>
+        <th align="center" className="items">
+          Joined On
+        </th>
+        <th align="center" className="items">
+          Department
+        </th>
+        <th align="center" className="items">
+          Access
+        </th>
+      </tr>
+    </thead>
+  );
+}
+
+function Content({ id, name, email, date, dept, admin, i }) {
+  return (
+    <tbody className="tableBody">
+      <tr className={`row tableBody ${i % 2 == 0 ? "odd" : "even"}`}>
+        <td align="center" className="items center">
+          {id}
+        </td>
+        <td align="center" className="items">
+          {name}
+        </td>
+        <td align="center" className="items">
+          {email}
+        </td>
+        <td align="center" className="items">
+          {date}
+        </td>
+        <td align="center" className="items">
+          {dept}
+        </td>
+        <td align="center" className={`items ${admin ? "admin" : "user"}`}>
+          {admin ? "Admin" : "User"}
+        </td>
+      </tr>
+    </tbody>
+  );
+}
+
 function Table() {
   return (
     <>
       <table className="table">
-        <thead className="row tableHead">
-          <tr>
-            <th align="center" className="items">
-              Id
-            </th>
-            <th align="center" className="items">
-              Name
-            </th>
-            <th align="center" className="items">
-              Email
-            </th>
-            <th align="center" className="items">
-              Joined On
-            </th>
-            <th align="center" className="items">
-              Department
-            </th>
-            <th align="center" className="items">
-              Access
-            </th>
-          </tr>
-        </thead>
+        <Head />
         {data.employees?.map((user, i) => (
-          <tbody key={user.empId} className="tableBody">
-            <tr className={`row tableBody ${i % 2 == 0 ? "odd" : "even"}`}>
-              <td align="center" className="items center">
-                {user.empId}
-              </td>
-              <td align="center" className="items">
-                {user.name}
-              </td>
-              <td align="center" className="items">
-                {user.email}
-              </td>
-              <td align="center" className="items">
-                {user.joinDate}
-              </td>
-              <td align="center" className="items">
-                {user.dept}
-              </td>
-              <td
-                align="center"
-                className={`items ${user.isAdmin ? "admin" : "user"}`}
-              >
-                {user.isAdmin ? "Admin" : "User"}
-              </td>
-            </tr>
-          </tbody>
+          <Content
+            id={user.empId}
+            name={user.name}
+            email={user.email}
+            date={user.joinDate}
+            dept={user.dept}
+            admin={user.isAdmin}
+            key={user.empId}
+            i={i}
+          />
         ))}
       </table>
     </>
